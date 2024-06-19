1f7b34ec4126e8c59c2acdb0f6e924234e371d13
pibouill <pibouill@student.42prague.com>
2024-06-19 17:57:43 +0200

    saving

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    app.py                                             :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: pibouill <pibouill@student.42prague.c      +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/06/19 17:54:00 by pibouill          #+#    #+#              #
+#    Updated: 2024/06/19 17:56:55 by pibouill         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 import os
@@ -15,7 +27,6 @@ def mention_handler(body: dict, say: callable):
     sender_id = f"<@{body.get('event', {}).get('user')}>"
     say(f"What's up? {sender_id}")
     bot_id = body.get("event", {}).get("text").split()[0]
-    # sender_submission = body.get("event", {}).get("text").split()[1]
     sender_submission = body.get("event", {}).get("text")
     sender_submission = sender_submission.replace(bot_id, "").strip()
     print(body)
@@ -33,7 +44,6 @@ def mention_handler(body: dict, say: callable):
         print("Not Good")
         say("nope.")
 	
-    # say('Hello World!')
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app, SLACK_APP_TOKEN)

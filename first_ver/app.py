cf31e922fde02a54809e0069220cfcd25f33f9ad
pibouill <pibouill@student.42prague.com>
2024-10-17 17:24:41 +0200

    saving first_ver updates. seems to be the easiest one to work with and the fastest as well

--- first_ver/app.py
+++ first_ver/app.py
@@ -1,14 +1,9 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-/*   app.py                                             :+:      :+:    :+:   */
-#                                                     +:+ +:+         +:+      #
-#    By: pibouill <pibouill@student.42prague.c      +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2024/10/09 13:11:29 by pibouill          #+#    #+#              #
-/*   Updated: 2024/10/09 13:11:40 by pibouill         ###   ########.fr       */
-#                                                                              #
-# **************************************************************************** #
+############
+############
+############
+############
+############
+############
 
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
@@ -24,16 +19,23 @@ SLACK_APP_TOKEN = os.environ['SLACK_APP_TOKEN']
 app = App(token=SLACK_BOT_TOKEN)
 
 @app.event("app_mention")
-def mention_handler(body: dict, say: callable):
+def mention_handler(body: dict, say):
+
     sender_id = f"<@{body.get('event', {}).get('user')}>"
+
     say(f"What's up? {sender_id}")
+
     bot_id = body.get("event", {}).get("text").split()[0]
     sender_submission = body.get("event", {}).get("text")
     sender_submission = sender_submission.replace(bot_id, "").strip()
     sending_time = body.get("event_time")
-    sending_time = datetime.datetime.utcfromtimestamp(sending_time)
+    if sending_time is not None:
+       # sending_time = datetime.datetime.fromtimestamp(sending_time, tz=datetime.timezone.utc)
+       sending_time = datetime.datetime.fromtimestamp(sending_time)
+    else:
+       sending_time = None
 
-    # print(body)
+    # print()
     print("\n-------------------------------------------------\n")
     print("\nbot_id-->", bot_id)
     print("sender_id-->", sender_id)
@@ -49,6 +51,17 @@ def mention_handler(body: dict, say: callable):
 	
     print("valid_input-->", valid_input)
     print("sending_time-->", sending_time)
+
+@app.event("message")
+def	handle_message_events(body, logger):
+	logger.info("received message event: %s", body)
+
+	# You can add more logic here to respond to general messages if needed
+    # For example, you might want to filter by user_id or channel_id
+    # and perform specific actions based on the message content.
+
+
+
 
 if __name__ == "__main__":
     valid_input = input("Your input: ")
